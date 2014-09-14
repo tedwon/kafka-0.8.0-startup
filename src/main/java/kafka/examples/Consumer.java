51a3cc2aeb86e31c783a05c6f021e3d9bee24387
tedwon <tedwon@daumcorp.com>
2014-09-14 20:18:39 +0900

    Update kafka version

--- src/main/java/kafka/examples/Consumer.java
+++ src/main/java/kafka/examples/Consumer.java
@@ -17,70 +17,63 @@
 package kafka.examples;
 
 
-import com.sun.javafx.collections.transformation.SortedList;
+import kafka.api.OffsetRequest;
+import kafka.consumer.ConsumerConfig;
+import kafka.consumer.KafkaStream;
+import kafka.javaapi.consumer.ConsumerConnector;
+import kafka.message.MessageAndMetadata;
 
-import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Properties;
 import java.util.TreeMap;
 
-import kafka.api.OffsetRequest;
-import kafka.consumer.ConsumerConfig;
-import kafka.consumer.ConsumerIterator;
-import kafka.consumer.KafkaStream;
-import kafka.javaapi.consumer.ConsumerConnector;
-import kafka.message.MessageAndMetadata;
-import scala.Option;
-import scala.collection.*;
-import scala.collection.Iterable;
-
 
 public class Consumer extends Thread {
 
-  private final ConsumerConnector consumer;
-  private final String topic;
+    private final ConsumerConnector consumer;
+    private final String topic;
 
-  public Consumer(String topic) {
-    consumer = kafka.consumer.Consumer.createJavaConsumerConnector(
-        createConsumerConfig());
-    this.topic = topic;
-  }
-
-  private static ConsumerConfig createConsumerConfig() {
-    Properties props = new Properties();
-    props.put("zookeeper.connect", KafkaProperties.zkConnect);
-    props.put("group.id", KafkaProperties.groupId);
-    props.put("zookeeper.session.timeout.ms", "400");
-    props.put("zookeeper.sync.time.ms", "200");
-    props.put("auto.commit.interval.ms", "1000");
-    props.put("auto.offset.reset", OffsetRequest.SmallestTimeString());
-
-    return new ConsumerConfig(props);
-
-  }
-
-  public void run() {
-    Map<String, Integer> topicCountMap = new HashMap<String, Integer>();
-    topicCountMap.put(topic, new Integer(4));
-    Map<String, List<KafkaStream<byte[], byte[]>>>
-        consumerMap =
-        consumer.createMessageStreams(topicCountMap);
-    List<KafkaStream<byte[], byte[]>> streams = consumerMap.get(topic);
-
-    System.out.println("size=" + streams.size());
-
-    Map<Integer, KafkaStream<byte[], byte[]>> map = new TreeMap();
-
-    for (KafkaStream<byte[], byte[]> stream : streams) {
-      MessageAndMetadata<byte[], byte[]> head = (MessageAndMetadata<byte[], byte[]>)stream.head();
-      int partition = head.partition();
-      System.out.println(partition);
-      map.put(partition, stream);
+    public Consumer(String topic) {
+        consumer = kafka.consumer.Consumer.createJavaConsumerConnector(
+                createConsumerConfig());
+        this.topic = topic;
     }
 
-    System.out.println(map);
+    private static ConsumerConfig createConsumerConfig() {
+        Properties props = new Properties();
+        props.put("zookeeper.connect", KafkaProperties.zkConnect);
+        props.put("group.id", KafkaProperties.groupId);
+        props.put("zookeeper.session.timeout.ms", "400");
+        props.put("zookeeper.sync.time.ms", "200");
+        props.put("auto.commit.interval.ms", "1000");
+        props.put("auto.offset.reset", OffsetRequest.SmallestTimeString());
+
+        return new ConsumerConfig(props);
+
+    }
+
+    public void run() {
+        Map<String, Integer> topicCountMap = new HashMap<String, Integer>();
+        topicCountMap.put(topic, new Integer(4));
+        Map<String, List<KafkaStream<byte[], byte[]>>>
+                consumerMap =
+                consumer.createMessageStreams(topicCountMap);
+        List<KafkaStream<byte[], byte[]>> streams = consumerMap.get(topic);
+
+        System.out.println("size=" + streams.size());
+
+        Map<Integer, KafkaStream<byte[], byte[]>> map = new TreeMap();
+
+        for (KafkaStream<byte[], byte[]> stream : streams) {
+            MessageAndMetadata<byte[], byte[]> head = (MessageAndMetadata<byte[], byte[]>) stream.head();
+            int partition = head.partition();
+            System.out.println(partition);
+            map.put(partition, stream);
+        }
+
+        System.out.println(map);
 
 //    KafkaStream<byte[], byte[]> stream = consumerMap.get(topic).get(0);
 //    ConsumerIterator<byte[], byte[]> it = stream.iterator();
@@ -96,5 +89,5 @@ public class Consumer extends Thread {
 //    KafkaStream<byte[], byte[]> stream = consumerMap.get(topic).get(1);
 //    String s = stream.clientId();
 //    System.out.println("###=" + s);
-  }
+    }
 }
